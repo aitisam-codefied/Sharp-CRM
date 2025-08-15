c80b8a6d9f8d77f28baed9b7a4e4062a80c329d8
aitisam@codefied <aitisam@codefied.co>
2025-08-15 16:32:26 -0700

    Update axios.ts

--- lib/axios.ts
+++ lib/axios.ts
@@ -3,8 +3,8 @@ import axios from "axios";
 // const BASE_URL = "http://localhost:5001/api/v1";
 const BASE_URL =
   process.env.NODE_ENV === "development"
-    ? "https://beta.supasystem.co.uk/api/v1"
-    : "https://test.supasystem.co.uk/api/v1";
+    ? "https://beta.api.supasystem.co.uk/api/v1"
+    : "https://test.api.supasystem.co.uk/api/v1";
 const api = axios.create({
   baseURL: BASE_URL,
   headers: {
