5bf4aaf9e2001ddca9f73c7e4ea9c2130b8a0131
aitisam@codefied <aitisam@codefied.co>
2025-08-18 12:23:00 -0700

    Update Axios Instance

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 // const BASE_URL = "http://localhost:5001/api/v1";
-const BASE_URL =
-  process.env.NODE_ENV === "development"
-    ? "https://beta.api.supasystem.co.uk/api/v1"
-    : "https://test.api.supasystem.co.uk/api/v1";
+const BASE_URL = "https://beta.api.supasystem.co.uk/api/v1"
 const api = axios.create({
   baseURL: BASE_URL,
   headers: {
