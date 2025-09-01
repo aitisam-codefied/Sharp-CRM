9dcf99da86f0e11e400350aefc720a76b18023a7
hamza-codefied <hamza.codefied123@gmail.com>
2025-09-01 13:09:05 -0500

    recent changes

--- lib/axios.ts
+++ lib/axios.ts
@@ -8,7 +8,7 @@ const api = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
-  withCredentials: true,
+  withCredentials: false,
 });
 
 // Request interceptor – attach access token
